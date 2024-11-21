50fc1c4f552f5e67b9d5f9c69473fffe62e9c4b0
minlearnminlearn <minlearn@minlearn.org>
2024-11-21 13:53:52 +0800

    Update SSTPServer.sh

--- SSTPServer.sh
+++ SSTPServer.sh
@@ -10,7 +10,7 @@ sudo apt-get install build-essential libssl-dev libwrap0-dev libpam0g-dev librea
 wget https://ghproxy.minlearn.org/api/https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.38-9760-rtm/softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
 
 # Extract and install SoftEther VPN server
-tar xzvf softether-vpnbridge-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
+tar xzvf softether-vpnserver-v4.38-9760-rtm-2021.08.17-linux-x64-64bit.tar.gz
 cd vpnserver
 sudo make
 
