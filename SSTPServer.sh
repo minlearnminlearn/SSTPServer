65c482f58963965c36e6eaa653b3abaf9a4018f6
minlearnminlearn <minlearn@minlearn.org>
2024-11-21 22:19:04 +0800

    Update SSTPServer.sh

--- SSTPServer.sh
+++ SSTPServer.sh
@@ -13,7 +13,7 @@ sudo apt-get update
 # sudo make
 
 sudo apt -y install git cmake gcc g++ make pkgconf libncurses5-dev libssl-dev libsodium-dev libreadline-dev zlib1g-dev
-git clone https://github.com/SoftEtherVPN/SoftEtherVPN.git
+git clone https://ghproxy.minlearn.org/api/https://github.com/SoftEtherVPN/SoftEtherVPN.git
 cd SoftEtherVPN
 git submodule init && git submodule update
 ./configure
